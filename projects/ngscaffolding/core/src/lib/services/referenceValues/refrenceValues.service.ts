2493af54cc3ba9247d0c512f02788d730f0899f9
Darren Baines <dbaines1@hotmail.com>
2023-04-06 16:11:17 +0100

    Core upgraded

--- projects/ngscaffolding/core/src/lib/services/referenceValues/refrenceValues.service.ts
+++ projects/ngscaffolding/core/src/lib/services/referenceValues/refrenceValues.service.ts
@@ -12,7 +12,7 @@ import { ReferenceValue } from '@ngscaffolding/models';
 import { SocketService } from '../socket/socket.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ReferenceValuesService {
   private className = 'ReferenceValuesService';
@@ -27,29 +27,36 @@ export class ReferenceValuesService {
     private logger: LoggingService,
     private socketService: SocketService
   ) {
-    socketService.cacheFlush$.subscribe(refValue => {
+    socketService.cacheFlush$.subscribe((refValue) => {
       this.clearReferenceValue(refValue, false);
     });
   }
 
   lookupValue(name: string, lookupValue: any): Observable<string> {
-    return new Observable<string>(observer => {
-        this.getReferenceValue(name)
-            .pipe(tap(refVal => {
-              const foundVal =refVal.referenceValueItems.find(val => val.value == lookupValue);
-              observer.next(refVal.referenceValueItems.find(val => val.value == lookupValue)?.display);
-              observer.complete();
-            }))
-            .subscribe();
+    return new Observable<string>((observer) => {
+      this.getReferenceValue(name)
+        .pipe(
+          tap((refVal) => {
+            const foundVal = refVal.referenceValueItems.find(
+              (val) => val.value == lookupValue
+            );
+            observer.next(
+              refVal.referenceValueItems.find((val) => val.value == lookupValue)
+                ?.display
+            );
+            observer.complete();
+          })
+        )
+        .subscribe();
     });
-}
+  }
 
   //
   // Get a single string value from References
   //
   getValue(name: string, group: string): Observable<string> {
-    return new Observable<string>(observer => {
-      this.getReferenceValue(name, group).subscribe(reference => {
+    return new Observable<string>((observer) => {
+      this.getReferenceValue(name, group).subscribe((reference) => {
         if (reference) {
           observer.next(reference.value);
         } else {
@@ -70,7 +77,10 @@ export class ReferenceValuesService {
     }
 
     for (const loopName of namesArray) {
-      const list = this.refValuesQuery.getAll({ filterBy: entity => entity.name.includes(loopName) });
+      const list = this.refValuesQuery.getAll({
+        filterBy: (entity) =>
+          entity.name.toLowerCase().includes(loopName.toLowerCase()),
+      });
       if (notifyOthers) {
         this.socketService.sendCacheClear(loopName);
       }
@@ -83,13 +93,20 @@ export class ReferenceValuesService {
 
   setReferenceValue(referenceValue: ReferenceValue) {
     referenceValue.compositeKey = this.getKey(referenceValue.name, '');
-    this.refValuesStore.upsert(this.getKey(referenceValue.name, ''), referenceValue);
+    this.refValuesStore.upsert(
+      this.getKey(referenceValue.name, ''),
+      referenceValue
+    );
   }
 
   //
   // Get a complex ReferenceValue (May include multiple values)
   //
-  getReferenceValue(name: string, seed = '', childDepth = 0): Observable<ReferenceValue> {
+  getReferenceValue(
+    name: string,
+    seed = '',
+    childDepth = 0
+  ): Observable<ReferenceValue> {
     if (this.refValuesQuery.hasEntity(this.getKey(name, seed))) {
       const cacheValue = this.refValuesQuery.getEntity(this.getKey(name, seed));
       if (this.isExpired(cacheValue)) {
@@ -99,19 +116,21 @@ export class ReferenceValuesService {
 
       // If we get one from Cache, thats handy to use
       this.logger.info(`Reference Values From Cache ${name}::${seed}`);
-      return new Observable<ReferenceValue>(observer => {
+      return new Observable<ReferenceValue>((observer) => {
         observer.next(this.refValuesQuery.getEntity(this.getKey(name, seed)));
         observer.complete();
       });
     } else if (childDepth > 0) {
       const refValue = this.refValuesQuery.getEntity(this.getKey(name, ''));
       if (refValue) {
-        const parentRef = refValue.referenceValueItems.find(parent => parent.value === seed);
+        const parentRef = refValue.referenceValueItems.find(
+          (parent) => parent.value === seed
+        );
         if (parentRef) {
           const clone = { ...refValue };
           clone.referenceValueItems = parentRef.referenceValueItems;
 
-          return new Observable<ReferenceValue>(observer => {
+          return new Observable<ReferenceValue>((observer) => {
             observer.next(clone);
             observer.complete();
           });
@@ -123,24 +142,28 @@ export class ReferenceValuesService {
     return of(null);
   }
 
-  private downloadRefValue(name: string, seed: string): Observable<ReferenceValue> {
+  private downloadRefValue(
+    name: string,
+    seed: string
+  ): Observable<ReferenceValue> {
     // Nothing in the Cache
 
     if (this.requestsInFlight.has(this.getKey(name, seed))) {
       // We have already asked for this, return our existing Observable
       return this.requestsInFlight.get(this.getKey(name, seed));
     } else {
-      const wrapper = new Observable<ReferenceValue>(observer => {
+      const wrapper = new Observable<ReferenceValue>((observer) => {
         // Call HTTP Here
         this.logger.info(`Reference Values From HTTP ${name}::${seed}`);
         const httpRequest = this.http
           .get<ReferenceValue>(
-            `${this.appSettingsService.getValue(AppSettings.apiHome)}/api/v1/referencevalues?name=${name}&seed=${seed}`
+            `${this.appSettingsService.getValue(
+              AppSettings.apiHome
+            )}/api/v1/referencevalues?name=${name}&seed=${seed}`
           )
           .pipe(timeout(40000), retry(1));
         httpRequest.subscribe(
-          value => {
-
+          (value) => {
             value.compositeKey = this.getKey(name, seed);
             value.whenStored = new Date();
 
@@ -150,11 +173,15 @@ export class ReferenceValuesService {
             observer.next(value);
             observer.complete();
           },
-          err => {
+          (err) => {
             // Error here. If we have a valid value, respond with that
             if (this.refValuesQuery.hasEntity(this.getKey(name, seed))) {
-              this.logger.info(`Reference Values From HTTP Failed using last Cache ${name}::${seed}`);
-              observer.next(this.refValuesQuery.getEntity(this.getKey(name, seed)));
+              this.logger.info(
+                `Reference Values From HTTP Failed using last Cache ${name}::${seed}`
+              );
+              observer.next(
+                this.refValuesQuery.getEntity(this.getKey(name, seed))
+              );
               observer.complete();
             } else {
               observer.error(err);
